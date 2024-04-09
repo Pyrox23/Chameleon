311d602257377700791055934d8b6ce4d4daf39f
Pyrox23 <amocoutte11@gmail.com>
2024-04-09 08:49:27 +0200

    .

--- Proyecto de Ingeniería/chameleon/src/app/gerente.java
+++ Proyecto de Ingeniería/chameleon/src/app/gerente.java
@@ -1,6 +1,7 @@
 package app;
+import java.io.Serializable;
 
-public class gerente extends empleado {
+public class gerente extends empleado implements Serializable{
 
 	//protected (para q el padre de gerente e hijo de gerente puedas acceder)
 
