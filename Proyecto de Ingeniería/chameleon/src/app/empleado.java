716f924ddd9e6d9a7312e71e315b83033f099eb7
Regina Franco <79554515+reginaafc@users.noreply.github.com>
2024-04-22 15:04:00 +0200

    Update empleado.java

--- Proyecto de Ingeniería/chameleon/src/app/empleado.java
+++ Proyecto de Ingeniería/chameleon/src/app/empleado.java
@@ -58,7 +58,69 @@ public class empleado extends usuario {
 		}
 
 	}
+	
+// Modificar inventario mientras no se haya cerrado
+	public void gestionarInventario(Scanner sin) {
+		boolean seguir = true;
+		int posicionProducto = 0;
+		String cambio = "";
+		String nombre = "";
+		String opcion = "";
 
+		// Bucle para modificar venta
+		do {
+			sin.nextLine(); // Consumir linea suelta
+			boolean encontrado = false;
+
+			// Imprimir ventas
+			System.out.println("\n ----- VENTAS ACTUALES: -----");
+			for (int j = 0; j < ventas.size(); j++) {
+				System.out.println(ventas.get(j));
+			}
+			
+			System.out.print("\n Indique el producto a modificar: ");
+			opcion = sin.nextLine();
+
+			// Comprobar que el producto existe
+			for (int i = 0; i < ventas.size(); i++) {
+				if ((ventas.get(i).getNombre()).equalsIgnoreCase(opcion)) {
+					posicionProducto = i;
+					encontrado = true;
+				}
+			}
+
+			if (encontrado == true) {
+				System.out.println(
+						" \n * Indique el cambio a realizar: \n \t a)Cambiar cantidad \n \t b)Eliminar");
+				cambio = sin.nextLine();
+
+				// Switchcito de cambios
+				nombre = ventas.get(posicionProducto).getNombre();
+				switch (cambio.toLowerCase()) {
+					case "a":
+						System.out.print("\n Ingrese la nueva cantidad de " + nombre + ": ");
+						ventas.get(posicionProducto).setCantidad(sin.nextInt());
+						break;
+
+					case "b":
+						System.out.print("\n Eliminando " + nombre + " de ventas... ");
+						ventas.remove(posicionProducto);
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			// Salir del bucle
+			System.out.print("\n ¿Desea modificar otro producto?: ");
+			if (sin.next().equalsIgnoreCase("No")) {
+				seguir = false;
+			}
+
+		} while (seguir);
+
+	}
 
   //Metodo para modificar las ventas del día
     public void mostrarVentas() {
